4cabca52989b0eae12c4ce5b4b97f8d1b19bdd10
Tigeriss <tigeriss@mail.ru>
2014-04-08 18:14:05 +0400

    setup env after crashing motherboard

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140327103521) do
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-
   create_table "news", force: true do |t|
     t.string   "title"
     t.string   "description"
